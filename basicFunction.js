c799658ec9ec70309be6984368085a0903a9ab7a
Petr Polansky <shikunek@gmail.com>
2018-04-25 13:11:36 +0200

    Snaha o ruzne stylovane cary

---
+++ basicFunction.js
@@ -0,0 +1,17 @@
+  // Initialize Firebase
+
+  function config() 
+  {
+      var config = {
+      apiKey: "AIzaSyAR7L3KrsOWXYqu0HMcM85EOBPKWSOF9t0",
+      authDomain: "testapp-5cce5.firebaseapp.com",
+      databaseURL: "https://testapp-5cce5.firebaseio.com",
+      projectId: "testapp-5cce5",
+      storageBucket: "",
+      messagingSenderId: "984038814608"
+    };
+    firebase.initializeApp(config);
+
+  }
+
+  
